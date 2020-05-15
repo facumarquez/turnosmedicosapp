a7fe08e79d9fe594af3ebea2ca6efae14846fb4f
sergio <gonzalez.sergiod@gmail.com>
2020-05-15 19:57:36 -0300

    agenda medico activity pantalla 1

---
+++ app/src/main/java/com/app/turnosapp/AgendaMedicoActivity.java
@@ -0,0 +1,44 @@
+package com.app.turnosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class AgendaMedicoActivity extends AppCompatActivity {
+    private Spinner meses;
+    private Spinner anios;
+    private Button verPerfil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda_medico);
+
+        meses = (Spinner)findViewById(R.id.spMes);
+        anios = (Spinner)findViewById(R.id.spAnio);
+        verPerfil = (Button)findViewById(R.id.btnPerfil);
+
+        ArrayAdapter<CharSequence> adapterMeses = ArrayAdapter.createFromResource(this,
+                R.array.meses_array, android.R.layout.simple_spinner_item);
+        adapterMeses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        meses.setAdapter(adapterMeses);
+
+        ArrayAdapter<CharSequence> adapterAnios = ArrayAdapter.createFromResource(this,
+                R.array.anios_array, android.R.layout.simple_spinner_item);
+        adapterAnios.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        anios.setAdapter(adapterAnios);
+
+        //Botones
+        verPerfil.setOnClickListener(new View.OnClickListener(){
+            public void onClick(android.view.View view){
+                Intent intent = new Intent(AgendaMedicoActivity.this, Usuario_verPerfil.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
