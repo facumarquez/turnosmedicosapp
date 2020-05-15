2304ee7ec38f4b10a3bc3633da795c8a59f4f4a6
sergio <gonzalez.sergiod@gmail.com>
2020-05-15 19:54:47 -0300

    Se renombra activity dado que se usa para para los 2 perfiles

--- app/src/main/java/com/app/turnosapp/Usuario_verPerfil.java
+++ app/src/main/java/com/app/turnosapp/Usuario_verPerfil.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class usuario_verPerfil extends AppCompatActivity {
+public class Usuario_verPerfil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
