56b6fcb168ce8ced46b878250eb9d125a9cd30eb
sergio <gonzalez.sergiod@gmail.com>
2020-05-27 14:50:24 -0300

    Se agregan llamadas...

--- app/src/main/java/com/app/turnosapp/AgendaMedicoActivity.java
+++ app/src/main/java/com/app/turnosapp/AgendaMedicoActivity.java
@@ -8,45 +8,165 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.app.turnosapp.Interface.AgendaMedicoService;
+import com.app.turnosapp.Interface.TurnosAPI;
+import com.app.turnosapp.Interface.UsuarioService;
+import com.app.turnosapp.Model.AgendaMedico;
+import com.app.turnosapp.Model.Usuario;
+
+import java.io.Serializable;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AgendaMedicoActivity extends AppCompatActivity {
-    private Spinner meses;
-    private Spinner anios;
-    private Button verAgenda;
-    private Button verPerfil;
+
+    private Spinner spMeses;
+    private Spinner spAnios;
+    private Button btnAgenda;
+    private Button btnPerfil;
+
+    private Usuario usuario;
+    private AgendaMedico agendaMedico;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda_medico);
 
-        meses = (Spinner)findViewById(R.id.spMes);
-        anios = (Spinner)findViewById(R.id.spAnio);
-        verAgenda = (Button)findViewById(R.id.btnAgenda);
-        verPerfil = (Button)findViewById(R.id.btnPerfil);
+        Intent intentLogin = getIntent();
+        //TODO: poner el id posta
+        long idUsuario = 1; //intent.getStringExtra("usuario");
+        obtenerUsuario(idUsuario);
+
+
+        spMeses = (Spinner) findViewById(R.id.spMes);
+        spAnios = (Spinner) findViewById(R.id.spAnio);
+        btnAgenda = (Button) findViewById(R.id.btnAgenda);
+        btnPerfil = (Button) findViewById(R.id.btnPerfil);
 
         ArrayAdapter<CharSequence> adapterMeses = ArrayAdapter.createFromResource(this,
                 R.array.meses_array, android.R.layout.simple_spinner_item);
         adapterMeses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        meses.setAdapter(adapterMeses);
+        spMeses.setAdapter(adapterMeses);
 
         ArrayAdapter<CharSequence> adapterAnios = ArrayAdapter.createFromResource(this,
                 R.array.anios_array, android.R.layout.simple_spinner_item);
         adapterAnios.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        anios.setAdapter(adapterAnios);
+        spAnios.setAdapter(adapterAnios);
+
 
         //Botones
-        verAgenda.setOnClickListener(new View.OnClickListener(){
-            public void onClick(android.view.View view){
+        btnAgenda.setOnClickListener(new View.OnClickListener() {
+            public void onClick(android.view.View view) {
+                long idMedico = usuario.getIdUsuario();
+                int mes = Integer.valueOf(spMeses.getSelectedItemPosition()+1);
+                int anio= Integer.valueOf(spAnios.getSelectedItem().toString());
+
+                obtenerAgenda(idMedico, mes, anio);
+
+                if (agendaMedico == null){
+                    //crearAgenda(idMedico, mes, anio);
+                }
+
                 Intent intent = new Intent(AgendaMedicoActivity.this, AgendaMedicoFechaActivity.class);
+                intent.putExtra("agendaMedico", (Serializable) agendaMedico);
                 startActivity(intent);
             }
         });
 
-        verPerfil.setOnClickListener(new View.OnClickListener(){
-            public void onClick(android.view.View view){
+        btnPerfil.setOnClickListener(new View.OnClickListener() {
+            public void onClick(android.view.View view) {
                 Intent intent = new Intent(AgendaMedicoActivity.this, Usuario_verPerfil.class);
+                intent.putExtra("usuario", (Serializable) usuario);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void obtenerUsuario(long idUsuario) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.apiTurnosURL))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        UsuarioService usuarioService = retrofit.create(UsuarioService.class);
+
+        Call<Usuario> call = usuarioService.getUsuarioPorID(idUsuario);
+        call.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AgendaMedicoActivity.this, "No se encontró el usuario", Toast.LENGTH_SHORT).show();
+                } else {
+                    usuario = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+                Toast.makeText(AgendaMedicoActivity.this, "Error al obtener el usuario", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void obtenerAgenda(long idMedico, int mes, int anio) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.apiTurnosURL))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AgendaMedicoService agendaMedicoService = retrofit.create(AgendaMedicoService.class);
+
+        Call<AgendaMedico> call = agendaMedicoService.getAgendaMedico(idMedico,mes,anio);
+        call.enqueue(new Callback<AgendaMedico>() {
+            @Override
+            public void onResponse(Call<AgendaMedico> call, Response<AgendaMedico> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AgendaMedicoActivity.this, "No se encontró la agenda", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AgendaMedicoActivity.this, "Se encontró la agenda", Toast.LENGTH_SHORT).show();
+                    agendaMedico = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AgendaMedico> call, Throwable t) {
+                Toast.makeText(AgendaMedicoActivity.this, "Error al obtener la agenda", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void crearAgenda(long idMedico, int mes, int anio) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.apiTurnosURL))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        AgendaMedicoService agendaMedicoService = retrofit.create(AgendaMedicoService.class);
+
+        Call<AgendaMedico> call = agendaMedicoService.crearAgendaMedico(agendaMedico);
+        call.enqueue(new Callback<AgendaMedico>() {
+            @Override
+            public void onResponse(Call<AgendaMedico> call, Response<AgendaMedico> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(AgendaMedicoActivity.this, "No se creó la agenda", Toast.LENGTH_SHORT).show();
+                } else {
+                    agendaMedico = response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AgendaMedico> call, Throwable t) {
+                Toast.makeText(AgendaMedicoActivity.this, "Error al crear la agenda", Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/app/turnosapp/AgendaMedicoFechaActivity.java
+++ app/src/main/java/com/app/turnosapp/AgendaMedicoFechaActivity.java
@@ -5,16 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.app.turnosapp.Interface.EspecialidadService;
+import com.app.turnosapp.Interface.MedicoService;
+import com.app.turnosapp.Interface.TurnosAPI;
+import com.app.turnosapp.Model.AgendaMedico;
+import com.app.turnosapp.Model.Especialidad;
+import com.app.turnosapp.Model.Usuario;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AgendaMedicoFechaActivity extends AppCompatActivity {
 
+    private AgendaMedico agendaMedico;
+    private Spinner especialidades;
+    private List<Especialidad> listaEspecialidades;
+    private ArrayList<String> listaFormateadaEspecialidades = new ArrayList<String>();
+
     private Button modHorarios;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fecha_medico);
+
+       Intent intentAgendaMedico = getIntent();
+       agendaMedico = (AgendaMedico)intentAgendaMedico.getSerializableExtra(("agendaMedico"));
+
+
+        especialidades = (Spinner) findViewById(R.id.spEspecialidad);
+        getEspecialidades();
 
         modHorarios = (Button)findViewById(R.id.buttonModHorarios);
 
@@ -25,6 +59,50 @@ public class AgendaMedicoFechaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    private void getEspecialidades(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getString(R.string.apiTurnosURL))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MedicoService medicoService = retrofit.create(MedicoService.class);
+        //TODO:poner id veradero
+        Call<List<Especialidad>> call = medicoService.getEspecialidadesPorMedico(1);
+        call.enqueue(new Callback<List<Especialidad>>() {
+            @Override
+            public void onResponse(Call<List<Especialidad>> call, Response<List<Especialidad>> response) {
+
+                if(!response.isSuccessful()) {
+                    Toast.makeText(AgendaMedicoFechaActivity.this, "ERROR: No se pudieron obtener las especialidades", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    listaEspecialidades = response.body();
+
+                    //Ordeno alfabeticamente las especilidades
+                    Collections.sort(listaEspecialidades, new Comparator<Especialidad>() {
+                        @Override
+                        public int compare(Especialidad o1, Especialidad o2) {
+                            return o1.getNombre().compareToIgnoreCase(o2.getNombre());
+                        }
+                    });
+
+                    //Creo un array auxiliar para guardar solamente los nombres y mostrarlos en el spinner
+                    for(Especialidad especialidad: listaEspecialidades){
+                        listaFormateadaEspecialidades.add(especialidad.getNombre());
+                    }
+
+                    //Cargo los datos en el Spinner
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(AgendaMedicoFechaActivity.this,android.R.layout.simple_spinner_item, listaFormateadaEspecialidades);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    especialidades.setAdapter(adapter);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Especialidad>> call, Throwable t) {
+                Toast.makeText(AgendaMedicoFechaActivity.this, "ERROR: No se pudieron obtener las especialidades", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
