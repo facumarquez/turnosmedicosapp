0677e3eed026d0125a9b64e35f71bbbb566e3189
sergio <gonzalez.sergiod@gmail.com>
2020-05-27 15:11:03 -0300

    Se arregla herencia de atributos en las entidades.

--- app/src/main/java/com/app/turnosapp/Model/Medico.java
+++ app/src/main/java/com/app/turnosapp/Model/Medico.java
@@ -2,81 +2,9 @@ package com.app.turnosapp.Model;
 
 import java.io.Serializable;
 
-public class Medico implements Serializable {
-    private int id_usuario;
+public class Medico extends Usuario implements Serializable {
+
     private String legajo;
-
-    public int getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getFecha_nacimiento() {
-        return fecha_nacimiento;
-    }
-
-    public void setFecha_nacimiento(String fecha_nacimiento) {
-        this.fecha_nacimiento = fecha_nacimiento;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 
     public String getLegajo() {
         return legajo;
@@ -89,20 +17,20 @@ public class Medico implements Serializable {
     @Override
     public String toString() {
         return "Medico{" +
-                "idUsuario=" + idUsuario +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", usuario='" + usuario + '\'' +
-                ", password='" + password + '\'' +
-                ", mail='" + mail + '\'' +
-                ", sexo='" + sexo + '\'' +
-                ", fecha_nacimiento='" + fecha_nacimiento + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", legajo='" + legajo + '\'' +
+                "idUsuario=" + this.getIdUsuario() +
+                ", nombre='" + this.getNombre() + '\'' +
+                ", apellido='" + this.getApellido() + '\'' +
+                ", usuario='" + this.getUsuario() + '\'' +
+                ", password='" + this.getPassword() + '\'' +
+                ", mail='" + this.getMail() + '\'' +
+                ", sexo='" + this.getSexo() + '\'' +
+                ", fecha_nacimiento='" + this.getFecha_nacimiento() + '\'' +
+                ", telefono='" + this.getTelefono() + '\'' +
+                ", legajo='" + this.getLegajo() + '\'' +
                 '}';
     }
 
     public String getFullname() {
-        return this.nombre+", "+this.apellido;
+        return this.getNombre() + ", " +this.getApellido();
     }
 }
--- app/src/main/java/com/app/turnosapp/Model/Paciente.java
+++ app/src/main/java/com/app/turnosapp/Model/Paciente.java
@@ -2,91 +2,10 @@ package com.app.turnosapp.Model;
 
 import java.util.ArrayList;
 
-public class Paciente {
+public class Paciente extends Usuario {
 
-    private Long id;
-    private String nombre;
-    private String apellido;
-    private String usuario;
-    private String password;
-    private String mail;
-    private String sexo;
-    private String fecha_nacimiento;
-    private String telefono;
     private String documento;
     private Boolean cuotaAlDia;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getFecha_nacimiento() {
-        return fecha_nacimiento;
-    }
-
-    public void setFecha_nacimiento(String fecha_nacimiento) {
-        this.fecha_nacimiento = fecha_nacimiento;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 
     public String getDocumento() {
         return documento;
@@ -107,17 +26,17 @@ public class Paciente {
     @Override
     public String toString() {
         return "Paciente{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", usuario='" + usuario + '\'' +
-                ", password='" + password + '\'' +
-                ", mail='" + mail + '\'' +
-                ", sexo='" + sexo + '\'' +
-                ", fecha_nacimiento='" + fecha_nacimiento + '\'' +
-                ", telefono='" + telefono + '\'' +
-                ", documento='" + documento + '\'' +
-                ", cuotaAlDia=" + cuotaAlDia +
+                "id=" + this.getIdUsuario() +
+                ", nombre='" + this.getNombre() + '\'' +
+                ", apellido='" + this.getApellido() + '\'' +
+                ", usuario='" + this.getUsuario() + '\'' +
+                ", password='" + this.getPassword() + '\'' +
+                ", mail='" + this.getMail() + '\'' +
+                ", sexo='" + this.getSexo() + '\'' +
+                ", fecha_nacimiento='" + this.getFecha_nacimiento() + '\'' +
+                ", telefono='" + this.getTelefono() + '\'' +
+                ", documento='" + this.getDocumento() + '\'' +
+                ", cuotaAlDia=" + this.getCuotaAlDia() +
                 '}';
     }
 }
