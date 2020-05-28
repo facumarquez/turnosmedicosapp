e217be939e266b62fbc89c27f7d9eb163134b4b9
sergio <gonzalez.sergiod@gmail.com>
2020-05-28 14:11:00 -0300

    Se da funcionalidad a boton actualizar y obtencion de datos

--- app/src/main/java/com/app/turnosapp/Usuario_verPerfil.java
+++ app/src/main/java/com/app/turnosapp/Usuario_verPerfil.java
@@ -7,20 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.app.turnosapp.Interface.TurnosAPI;
+import com.app.turnosapp.Helpers.RetrofitConnection;
 import com.app.turnosapp.Interface.UsuarioService;
 import com.app.turnosapp.Model.Usuario;
-
-import java.io.Serializable;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Usuario_verPerfil extends AppCompatActivity {
 
@@ -28,7 +23,6 @@ public class Usuario_verPerfil extends AppCompatActivity {
     private EditText etTelefono;
     private Button btnAtras;
     private Button btnActualizarDatos;
-    //private String strUsuario;
     private Usuario nuevoUsuario;
 
     @Override
@@ -37,9 +31,6 @@ public class Usuario_verPerfil extends AppCompatActivity {
         setContentView(R.layout.activity_usuario_ver_perfil);
 
         Intent intentAgendaMedico = getIntent();
-
-        //strUsuario = intentAgendaMedico.getStringExtra("usuario");
-        //obtenerUsuario(strUsuario);
         nuevoUsuario = (Usuario)intentAgendaMedico.getSerializableExtra(("usuario"));
 
         etMail = (EditText) findViewById(R.id.etEmail);
@@ -56,31 +47,21 @@ public class Usuario_verPerfil extends AppCompatActivity {
                 nuevoUsuario.setMail(etMail.getText().toString().trim());
                 nuevoUsuario.setTelefono((etTelefono.getText().toString().trim()));
                 actualizarUsuario(nuevoUsuario.getIdUsuario());
-                etMail.setText(nuevoUsuario.getMail().trim());
-                etTelefono.setText(nuevoUsuario.getTelefono().trim());
                 finish();
-                //Intent intent = new Intent(Usuario_verPerfil.this, AgendaMedicoActivity.class);
-                //startActivity(intent);
             }
         });
 
         btnAtras.setOnClickListener(new View.OnClickListener() {
             public void onClick(android.view.View view) {
                 finish();
-                //Intent intent = new Intent(Usuario_verPerfil.this, AgendaMedicoActivity.class);
-                //startActivity(intent);
             }
         });
-
     }
 
     private void actualizarUsuario(long idUsuario) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(getString(R.string.apiTurnosURL))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        UsuarioService usuarioService = retrofit.create(UsuarioService.class);
+        UsuarioService usuarioService = RetrofitConnection.obtenerConexion
+                (getString(R.string.apiTurnosURL)).create(UsuarioService.class);
 
         Call<Usuario> call = usuarioService.actualizarUsuario(idUsuario, nuevoUsuario);
         call.enqueue(new Callback<Usuario>() {
@@ -100,32 +81,4 @@ public class Usuario_verPerfil extends AppCompatActivity {
             }
         });
     }
-
-    /*
-    private void obtenerUsuario(String nombreUsuario) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(getString(R.string.apiTurnosURL))
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        UsuarioService usuarioService = retrofit.create(UsuarioService.class);
-
-        Call<Usuario> call = usuarioService.getUsuarioPorNombre(nombreUsuario);
-        call.enqueue(new Callback<Usuario>() {
-            @Override
-            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
-                if (!response.isSuccessful()) {
-                    Toast.makeText(Usuario_verPerfil.this, "No se encontró el usuario", Toast.LENGTH_SHORT).show();
-                } else {
-                    nuevoUsuario = response.body();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Usuario> call, Throwable t) {
-                Toast.makeText(Usuario_verPerfil.this, "Error al obtener el usuario", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-    */
 }
