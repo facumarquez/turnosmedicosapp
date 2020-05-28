0a52344b9722c30741609ea7d53ea0a887f67bf9
sergio <gonzalez.sergiod@gmail.com>
2020-05-28 16:20:24 -0300

    Se agrega llamada

--- app/src/main/java/com/app/turnosapp/Interface/MedicoService.java
+++ app/src/main/java/com/app/turnosapp/Interface/MedicoService.java
@@ -1,6 +1,7 @@
 package com.app.turnosapp.Interface;
 
 import com.app.turnosapp.Model.Especialidad;
+import com.app.turnosapp.Model.Medico;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ import retrofit2.http.Path;
 public interface MedicoService {
     @GET("Medicos/{idMedico}/especialidades")
     Call<List<Especialidad>> getEspecialidadesPorMedico(@Path("idMedico") long idMedico);
+
+    @GET("Medicos/NombreUsuario/{nombreUsuario}")
+    Call<Medico> getMedicoPorNombre(@Path("nombreUsuario") String nombreUsuario);
 }
